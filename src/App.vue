bb24883555fd89cf3fce618ea48625c0e31a3954
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-01-05 00:00:12 +0100

    Get frontend working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <ais-index index-name="products" :search-store="searchStore">
+    <div class="CreateProductForm" style="margin: 10px;">
+      <ais-input style="background:white;" placeholder="Search products..."></ais-input>
+    </div>
+    <ais-results v-if="searchStore.query.length > 0">
+      <template slot-scope="{ query, result }">
+        {{ query }}
+        <span v-if="query != ''" style="margin: 10px;width: 808px; transition: opacity 480ms cubic-bezier(0.165, 0.84, 0.44, 1) 0s, transform 480ms cubic-bezier(0.165, 0.84, 0.44, 1) 0s; transform: translateX(0px) translateY(278px);">
+          <div class="card">
+            <div class="card-content">
+              <article class="media">
+                <div class="media-left">
+                  <a :href="'https://getmakerlog.com/products/' + result.slug" target="_blank">
+                    <figure v-if="result.icon" class="image is-48x48 img-rounded"><img :src="result.icon" :alt="result.name"></figure>
+                  </a>
+                </div>
+                <div class="media-content"><a :href="'https://getmakerlog.com/products/' + result.slug" target="_blank">
+                    <h1 class="title is-5">
+                      {{ result.name }}
+                      <a v-if="result.launched && result.product_hunt != ''" :href="result.product_hunt" target="_blank"
+                        style="text-decoration: none" class="tag">
+                        <span class="emoji" role="img">
+                          <img alt="🚀" draggable="false" src="https://twemoji.maxcdn.com/2/72x72/1f680.png" style="height: 1em; width: 1em; margin: 0px 0.05em 0px 0.1em; vertical-align: -0.1em;">
+                        </span>
+                        <span>Launched</span>
+                      </a>
+                      <span v-else-if="result.launched && result.product_hunt == ''" class="tag">
+                        <span class="emoji" role="img">
+                          <img alt="🚀" draggable="false" src="https://twemoji.maxcdn.com/2/72x72/1f680.png" style="height: 1em; width: 1em; margin: 0px 0.05em 0px 0.1em; vertical-align: -0.1em;">
+                        </span>
+                        <span>Launched</span>
+                      </span>
+                    </h1>
+                    <h5 class="subtitle is-6">{{ result.description }}</h5>
+                  </a></div>
+              </article>
+            </div>
+            <footer v-if="result.website != ''" class="card-footer">
+              <p class="card-footer-item">
+                <a :href="result.website" target="_blank" rel="noopener noreferrer">
+                  <button class="button is-small is-text">
+                    <span class="icon">
+                      <svg aria-hidden="true" data-prefix="fas" data-icon="globe" class="svg-inline--fa fa-globe fa-w-16 "
+                        role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+                        <path fill="currentColor" d="M336.5 160C322 70.7 287.8 8 248 8s-74 62.7-88.5 152h177zM152 256c0 22.2 1.2 43.5 3.3 64h185.3c2.1-20.5 3.3-41.8 3.3-64s-1.2-43.5-3.3-64H155.3c-2.1 20.5-3.3 41.8-3.3 64zm324.7-96c-28.6-67.9-86.5-120.4-158-141.6 24.4 33.8 41.2 84.7 50 141.6h108zM177.2 18.4C105.8 39.6 47.8 92.1 19.3 160h108c8.7-56.9 25.5-107.8 49.9-141.6zM487.4 192H372.7c2.1 21 3.3 42.5 3.3 64s-1.2 43-3.3 64h114.6c5.5-20.5 8.6-41.8 8.6-64s-3.1-43.5-8.5-64zM120 256c0-21.5 1.2-43 3.3-64H8.6C3.2 212.5 0 233.8 0 256s3.2 43.5 8.6 64h114.6c-2-21-3.2-42.5-3.2-64zm39.5 96c14.5 89.3 48.7 152 88.5 152s74-62.7 88.5-152h-177zm159.3 141.6c71.4-21.2 129.4-73.7 158-141.6h-108c-8.8 56.9-25.6 107.8-50 141.6zM19.3 352c28.6 67.9 86.5 120.4 158 141.6-24.4-33.8-41.2-84.7-50-141.6h-108z"></path>
+                      </svg>
+                    </span>
+                    <span>{{ result.website }}</span>
+                  </button>
+                </a>
+              </p>
+              <!-- <p class="card-footer-item">
+                                        <a class="unstyled-link" href="/@kerr">
+                                            <div class="heading">Made by <u><strong>@kerr</strong></u></div>
+                                        </a>
+                                    </p> -->
+            </footer>
+          </div>
+        </span>
+      </template>
+    </ais-results>
+  </ais-index>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+  import { createFromAlgoliaCredentials } from "vue-instantsearch";
+  const searchStore = createFromAlgoliaCredentials('X2AXEBEN1X', '941b400209e4c29d834f6c7793d3d2a5');
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  data: () => {
+    return {
+      search: "",
+      searchStore
+    };
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+*/body,figure,h1,h5,html,p{margin:0;padding:0}h1,h5{font-size:100%;font-weight:400}button,input{margin:0}html{box-sizing:border-box}*,:after,:before{box-sizing:inherit}img{max-width:100%}html{background-color:#fff;font-size:16px;-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;min-width:300px;overflow-x:hidden;overflow-y:scroll;text-rendering:optimizeLegibility;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;text-size-adjust:100%}article,figure,footer{display:block}body,button,input{font-family:BlinkMacSystemFont,-apple-system,Segoe
+UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica
+Neue,Helvetica,Arial,sans-serif}body{color:#4a4a4a;font-size:1rem;font-weight:400;line-height:1.5}a{color:#53af87;cursor:pointer;text-decoration:none}a:hover{color:#363636}img{height:auto;max-width:100%}span{font-style:inherit;font-weight:inherit}.button{-moz-appearance:none;-webkit-appearance:none;align-items:center;border-radius:3px;box-shadow:none;display:inline-flex;font-size:1rem;height:2.25em;justify-content:flex-start;line-height:1.5;position:relative;vertical-align:top;-webkit-touch-callout:none;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;background-color:#fff;border:1px
+solid #dbdbdb;color:#363636;cursor:pointer;justify-content:center;padding:calc(.375em - 1px)
+.75em;text-align:center;white-space:nowrap}.button:active,.button:focus{outline:0}.button
+.icon{height:1.5em;width:1.5em}.button .icon:first-child:not(:last-child){margin-left:calc(-.375em -
+1px);margin-right:.1875em}.button:hover{border-color:#b5b5b5;color:#363636}.button:focus{border-color:#3273dc;color:#363636}.button:focus:not(:active){box-shadow:0
+0 0 .125em
+rgba(83,175,135,.25)}.button:active{border-color:#4a4a4a;color:#363636}.button.is-text{background-color:initial;border-color:transparent;color:#4a4a4a;text-decoration:underline}.button.is-text:focus,.button.is-text:hover{background-color:#f5f5f5;color:#363636}.button.is-text:active{background-color:#e8e8e8;color:#363636}.button.is-small{border-radius:2px;font-size:.75rem}.icon{align-items:center;display:inline-flex;justify-content:center;height:1.5rem;width:1.5rem}.image{display:block;position:relative}.image
+img{display:block;height:auto;width:100%}.image.is-48x48{height:48px;width:48px}.tag:not(body){align-items:center;background-color:#f5f5f5;border-radius:3px;color:#4a4a4a;display:inline-flex;font-size:.75rem;height:2em;justify-content:center;line-height:1.5;padding-left:.75em;padding-right:.75em;white-space:nowrap}a.tag:hover{text-decoration:underline}.subtitle,.title{word-break:break-word}.title:not(:last-child){margin-bottom:1.5rem}.title
+span{font-weight:inherit}.title
+.tag{vertical-align:middle}.title{color:#363636;font-size:2rem;font-weight:600;line-height:1.125}.title:not(.is-spaced)+.subtitle{margin-top:-1.5rem}.title.is-5{font-size:1.25rem}.subtitle{color:#4a4a4a;font-size:1.25rem;font-weight:400;line-height:1.25}.subtitle.is-6{font-size:1rem}.card{background-color:#fff;box-shadow:0
+2px 3px rgba(10,10,10,.1),0 0 0 1px
+rgba(10,10,10,.1);color:#4a4a4a;max-width:100%;position:relative}.card-content{padding:1.5rem}.card-footer{border-top:1px
+solid
+#dbdbdb;align-items:stretch;display:flex}.card-footer-item{align-items:center;display:flex;flex-basis:0;flex-grow:1;flex-shrink:0;justify-content:center;padding:.75rem}.media{align-items:flex-start;display:flex;text-align:left}.media-left{flex-basis:auto;flex-grow:0;flex-shrink:0}.media-left{margin-right:1rem}.media-content{flex-basis:auto;flex-grow:1;flex-shrink:1;overflow:auto;text-align:left}html{background-color:#ecf0f1!important}body{font-family:sans-serif!important}h1{font-family:Poppins,sans-serif;font-weight:700!important}.media-content{overflow:hidden!important}.card{background-color:#fff!important;border:1px
+solid
+#fff!important;border-radius:8px!important;box-shadow:none!important}.button{border-radius:5px}.img-rounded,.img-rounded>img{border-radius:5px}.button>.icon{margin-right:3px!important}.CreateProductForm{width:100%}.CreateProductForm
+input{width:100%;background-color:#f1f2f6;border:none;outline:0;border-radius:6px;font-weight:500;box-shadow:none;font-size:2em;padding:10px}.CreateProductForm
+input::-webkit-input-placeholder{color:#d3d3d3}.CreateProductForm input::-ms-input-placeholder{color:#d3d3d3}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+import InstantSearch from "vue-instantsearch";
 
 Vue.config.productionTip = false;
+Vue.use(InstantSearch);
 
 new Vue({
   render: h => h(App)
